d535786f3fd760efbe24ae6bfe0e863778d6e458
Daniellexzd <36654876+Daniellexzd@users.noreply.github.com>
2025-02-12 05:01:57 +0000

    Deploying to gh-pages from @ Daniellexzd/Daniellexzd.github.io@8f6143da5b6baa80770a6f93e17e3f25dbfe86d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -403,6 +403,26 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
