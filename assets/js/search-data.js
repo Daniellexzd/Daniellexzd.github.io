23960d0cd7568df9b81a8c5c51e5a7b3d3b3b526
Daniellexzd <36654876+Daniellexzd@users.noreply.github.com>
2025-02-12 04:56:39 +0000

    Deploying to gh-pages from @ Daniellexzd/Daniellexzd.github.io@50d9291d47bcd1297f0fac00a9ac97ec186af24c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "A list of courses I have taught.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "Please email me for my full cv.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "post-a-post-with-image-galleries",
       
