ef6501c5c8b52cc9120360f0eeda7044676ce8f6
Daniellexzd <36654876+Daniellexzd@users.noreply.github.com>
2025-02-12 04:49:10 +0000

    Deploying to gh-pages from @ Daniellexzd/Daniellexzd.github.io@e1545a9c1e11bd2f77f4d8b14d395efa28ed0358 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,7 +431,10 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "projects-project-1",
+    },{id: "news-a-first-author-paper-on-audio-visual-processing-has-been-published-in-plos-one",
+          title: 'A first-author paper on audio-visual processing has been published in PLOS One!',
+          description: "",
+          section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
